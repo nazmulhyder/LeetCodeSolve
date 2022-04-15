5d31969563f073336377308214c6936b9f74db3e
nazmulhyder <nhyder.cse131.uiu@gmail.com>
2022-04-15 23:22:50 +0600

    ADD more

--- leetcode_solve/Es_28_Implement_strStr.cs
+++ leetcode_solve/Es_28_Implement_strStr.cs
@@ -6,5 +6,57 @@ namespace leetcode_solve
 {
     internal class Es_28_Implement_strStr
     {
+        public static void Main(string[] args)
+        {
+            string haystack = "a";
+            string needle = "a";
+            Console.WriteLine("Result: {0}", StrStr(haystack,needle));
+        }
+        public static int StrStr(string haystack, string needle)
+        {
+
+            var test = haystack.IndexOf(needle);
+
+            int index = 0;
+            int storeIndex = 0;
+            bool flag = false;
+            var tempNeedle = needle;
+
+            if (haystack.Length == 1 && needle.Length == 1 && haystack[0] == needle[0]) return 0;
+            if (haystack.Length == 1 && needle.Length == 1 && haystack[0] != needle[0]) return -1;
+            if (needle.Length == 1 && haystack[0] == needle[0]) return 0;
+            if (haystack.Equals(needle)) return 0;
+            if(needle.Length == 1 && !haystack.Contains(needle)) return -1;
+            if (needle.Length > haystack.Length) return -1;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {        
+                if (String.IsNullOrWhiteSpace(needle) || String.IsNullOrWhiteSpace(needle)) return -1;
+
+                //var tracIndex = index;
+                //var tracNeedleLen = needle.Length - 1;
+                //var tracHaystackChar = haystack[i];
+                //var tracNeedleChar = needle[index];
+                //var tracTempNeedle = tempNeedle;
+
+                if (!String.IsNullOrEmpty(tempNeedle) && haystack[i] == needle[index] && index <= needle.Length-1)
+                {
+                    if(index == 0)
+                    storeIndex = i;
+                    tempNeedle = tempNeedle.Substring(0, (needle.Length - 1) - index);
+                    index = index < needle.Length-1 ? index+1 : index;
+                }
+                else
+                {
+                    index = 0;
+                }
+
+                if (index == (needle.Length - 1))
+                {
+                    flag = true;
+                }
+            } 
+            return index >= 0 && flag ? storeIndex : -1;
+        }
     }
 }
---
+++ leetcode_solve/Es_28_Implement_strStr_impl2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode_solve
+{
+    internal class Es_28_Implement_strStr_impl2
+    {
+        public static void Main(string[] args)
+        {
+            string haystack = "mississippi";
+            string needle = "issip";
+            Console.WriteLine("Result: {0}", StrStr(haystack, needle));
+        }
+        public static int StrStr(string haystack, string needle)
+        {
+            if (String.IsNullOrWhiteSpace(needle) || String.IsNullOrWhiteSpace(needle)) return -1;
+            return haystack.IndexOf(needle);
+        }
+    }
+}
