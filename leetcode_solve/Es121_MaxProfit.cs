4ae21ece47c017586b094389de6d5284661e4ed0
nazmul.hyder <nazmul.hyder@synesisit.info>
2023-12-31 21:37:40 +0600

    valid palindrome [pass]

---
+++ leetcode_solve/Es121_MaxProfit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode_solve
+{
+    public class Es121_MaxProfit
+    {
+        public static void Main(string[] args)
+        {
+            int[] prices = new int[] { 1, 4, 2 };
+            Console.WriteLine(MaxProfit(prices)); 
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+           int buyIndex = 0; int minValue = prices[0]; int[] minInd = new int[1]; int[] maxInd = new int[1];
+           int maxVal = -999999; int sellIndex = 0;
+
+            for (int i = 0; i< prices.Length; i++)
+            {
+                //minValue = minValue>= prices[i] ? prices[i] :  minValue; 
+                buyIndex = (i== prices.Length-1? i: i+1);
+
+                if (minValue > prices[buyIndex])
+                {
+                    minValue = prices[buyIndex];
+                    minInd[0] = minValue;
+                }
+                else
+                {
+                    minInd[0] = minValue;
+                }
+            }
+
+            for (int i =  Array.FindIndex(prices, c => c.Equals(minInd[0])) +1; i < prices.Length; i++)
+            {
+                maxVal = maxVal < prices[i]? prices[i] : maxVal ; 
+                sellIndex = (i == prices.Length - 1 ? i : i + 1);
+
+                if (maxVal < prices[sellIndex])
+                {
+                    maxVal = prices[sellIndex];
+                    maxInd[0] = prices[sellIndex];
+                }
+                else
+                {
+                    maxInd[0] = maxVal;
+                }
+            }
+
+            return  maxInd[0] - minInd[0] <= 0? 0 : maxInd[0] - minInd[0];
+        }
+    }
+}
---
+++ leetcode_solve/Es125_ValidPalindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace leetcode_solve
+{
+    public class Es125_ValidPalindrome
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter string.");
+            string str = Console.ReadLine();
+            Console.WriteLine(IsValidPalindrome(str));
+        }
+
+        public static bool IsValidPalindrome(string palindrome)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            palindrome = rgx.Replace(palindrome, "");
+            palindrome= palindrome.Replace(" ", ""); palindrome = palindrome.Replace("-", "");
+            var res =  palindrome.ToLower().Equals(StringReverse(palindrome).ToLower()) ? true : false;
+            return res;
+        }
+
+        public static string StringReverse(string str)
+
+        {
+            char[] convCharArray = str.ToCharArray();
+            Array.Reverse(convCharArray);
+            return new string(convCharArray);
+        }
+    }
+}
